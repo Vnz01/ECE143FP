234ef8958587e34485274db55c17a7802fea0413
Venz Burgos <venz.burgos@yahoo.com>
2023-11-10 22:25:25 -0800

    Final Project Started

--- Python Code/DataPull.py
+++ Python Code/DataPull.py
@@ -1,3 +1,41 @@
+import matplotlib.pyplot as plt
 import numpy
-import matplotlib
 import pandas
+import csv
+
+data = []
+country = 'United Kingdom'
+
+with open('../Downloaded Database/monthly.csv','r') as monthlyCSV:
+    csv_reader = csv.reader(monthlyCSV)
+    for row in csv_reader:
+        if(row[1] == country):
+            data.append(row)
+
+dates = [entry[0] for entry in data]
+exchange_rates = [float(entry[2]) for entry in data]
+
+fig, ax = plt.subplots()
+
+country = country + ' Exchange Rate with USD'
+
+ax.plot(dates, exchange_rates, label=country, marker='o')
+
+num_ticks = 10
+
+step = len(dates) // num_ticks
+
+date_labels = dates[::step]
+
+ax.set_xticks(date_labels)
+
+plt.xticks(rotation=45)
+
+ax.set_xlabel('Dates')
+ax.set_ylabel('Exchange Rate')
+ax.set_title(country)
+
+ax.legend()
+
+plt.tight_layout()
+plt.show()
