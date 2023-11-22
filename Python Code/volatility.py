84eba5b54b2995e5abee5dad5f42f5cdc7881c37
Venz Burgos <venz.burgos@yahoo.com>
2023-11-22 13:14:23 -0800

    decade data

--- Python Code/volatility.py
+++ Python Code/volatility.py
@@ -2,9 +2,17 @@ import pandas as pd
 
 ### Change File Path for each dataset
 
+'../Downloaded Database/E2U(MinuteData2012).csv'
+'../Downloaded Database/E2U(MinuteData2013).csv'
+'../Downloaded Database/E2U(MinuteData2014).csv'
+'../Downloaded Database/E2U(MinuteData2015).csv'
+'../Downloaded Database/E2U(MinuteData2016).csv'
 '../Downloaded Database/E2U(MinuteData2017).csv'
+'../Downloaded Database/E2U(MinuteData2018).csv'
+'../Downloaded Database/E2U(MinuteData2019).csv'
 '../Downloaded Database/E2U(MinuteData2020).csv'
 '../Downloaded Database/E2U(MinuteData2021).csv'
+'../Downloaded Database/E2U(MinuteData2022).csv'
 
 data = pd.read_csv('../Downloaded Database/E2U(MinuteData2020).csv', header=None, names=['date', 'time', 'open', 'high', 'low', 'close', 'volume'])
 
